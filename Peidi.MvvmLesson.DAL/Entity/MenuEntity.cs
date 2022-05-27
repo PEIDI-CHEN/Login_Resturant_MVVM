a4c15a231b6f66aee1bd4ab3e7f0559bba381303
Peidi Chen <91669751+PEIDI-CHEN@users.noreply.github.com>
2022-05-27 00:25:14 +0000

    Add files via upload

---
+++ Peidi.MvvmLesson.DAL/Entity/MenuEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Peidi.MvvmLesson.DAL.Entity
+{
+    public class MenuEntity
+    {
+        /// 菜单ID
+        /// 菜单名称
+        /// 目标视图
+        /// 父节点ID
+        /// 
+        public string MenuID { get; set; }
+        public string MenuName { get; set; }
+        public string TargetView { get; set; }
+        public string ParentID { get; set; }
+    }
+}
---
+++ Peidi.MvvmLesson.DAL/SqlServerDataAccess.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Peidi.MvvmLesson.DAL.Entity;
+
+namespace Peidi.MvvmLesson.DAL
+{
+    public class SqlServerDataAccess
+    {
+
+        //SqlConnetion
+
+        public List<MenuEntity> GetMenus()
+        {
+            /// 菜单ID
+            /// 菜单名称
+            /// 目标视图
+            /// 父节点ID
+            /// 
+            List<MenuEntity> menuList = new List<MenuEntity>();
+            menuList.Add(new MenuEntity
+            {
+                MenuID = "1001",
+                MenuName = "Dashboard",
+                TargetView = "Peidi.MvvmLesson.View|Peidi.MvvmLesson.View.DashboardPage",
+                ParentID = "0"
+            });
+            menuList.Add(new MenuEntity
+            {
+                MenuID = "1002",
+                MenuName = "Restaurants",
+                TargetView = "Peidi.MvvmLesson.View|Peidi.MvvmLesson.View.RestaurantsPage",
+                ParentID = "0"
+            });
+            menuList.Add(new MenuEntity
+            {
+                MenuID = "1003",
+                MenuName = "Customers",
+                TargetView = "",
+                ParentID = "0"
+            });
+            menuList.Add(new MenuEntity
+            {
+                MenuID = "1004",
+                MenuName = "Delivery Partners",
+                TargetView = "",
+                ParentID = "0"
+            });
+            menuList.Add(new MenuEntity
+            {
+                MenuID = "1005",
+                MenuName = "Promotions",
+                TargetView = "",
+                ParentID = "0"
+            });
+            menuList.Add(new MenuEntity
+            {
+                MenuID = "1006",
+                MenuName = "Settings",
+                TargetView = "",
+                ParentID = "0"
+            });
+            menuList.Add(new MenuEntity
+            {
+                MenuID = "1007",
+                MenuName = "Notifycations",
+                TargetView = "",
+                ParentID = "0"
+            });
+            return menuList;
+        }
+    }
+}
