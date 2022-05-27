9baf6a9fcfe43b20849ebf8cd210a3b3d985773d
Peidi Chen <91669751+PEIDI-CHEN@users.noreply.github.com>
2022-05-27 00:21:51 +0000

    Add files via upload

---
+++ Peidi.MvvmLesson/App.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Peidi.MvvmLesson.View;
+
+namespace Peidi.MvvmLesson
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // 异常处理
+            this.DispatcherUnhandledException += App_DispatcherUnhandledException;
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+
+            LoginWindow lwin = new LoginWindow();
+            if (lwin.ShowDialog() == true)
+            {
+                new MainWindow().ShowDialog();
+            }
+
+            Application.Current.Shutdown();
+        }
+
+        private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            
+        }
+
+        private void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            
+        }
+    }
+}
